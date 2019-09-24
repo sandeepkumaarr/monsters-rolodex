5434b9891402ceb535bc3bbc290d51aa232c5f92
Sandeep Kumar <SandeepKumarr@protonmail.com>
2019-09-24 23:09:10 +0530

    Updates

---
+++ precache-manifest.83711a5736f5f5b1f4baba696ac3f214.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5108c4857f8ee7c7126328f78c02b5e6",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "dc59561e023a12c06038",
+    "url": "/monsters-rolodex/static/css/main.f0527b6b.chunk.css"
+  },
+  {
+    "revision": "d81979aa8ac68af22713",
+    "url": "/monsters-rolodex/static/js/2.4ea1ec15.chunk.js"
+  },
+  {
+    "revision": "dc59561e023a12c06038",
+    "url": "/monsters-rolodex/static/js/main.46f509b2.chunk.js"
+  },
+  {
+    "revision": "c7f991e6cb4746d6bd30",
+    "url": "/monsters-rolodex/static/js/runtime~main.205b164d.js"
+  }
+]);
